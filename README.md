53277454a7935eeb09def914c8c2f91671eea14c
Maxence Pastor <23473052+maxpastor@users.noreply.github.com>
2023-11-15 15:35:33 +0000

    the code and doc

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
 # Amazon-Review-Analysis
 Simple Amazon Bedrock demo showing how to extract key elements from Amazon Reviews
+
+## Disclamer
+This code is designed for demonstration purposes and may not adhere to standard best practices.
+Authored by Maxence Pastor, this code is a personal project and not owned by Amazon Web Services.
+Neither Amazon nor I hold liability for any issues arising from this code.
+Please note, executing this code may incur costs on your account.
+
+## Goal
+
+The goal is simple: demonstrate how to make a simple Amazon review parser / analyser using Amazon Bedrock. 
+
+Here is an example: 
+
+![Screenshot](resources/screenshot.png)
+
+## How to use 
+
+- 1: Create a small Cloud9 instance on your AWS account
+- 2: Ask for access to Amazon Bedrock models in your region (approval is instant)
+- 3: Create a role for your Cloud9 instance with the right permissions (Amazon Bedrock), don't forget to follow the principle of least privilege
+- 4: Assign the role to the instance in the EC2 console (top right-> actions -> security -> Modify IAM role)
+- 5: In Cloud9, remove the AWS managed credentials (top right -> gear icon -> AWS Settings -> Credentials -> Turn Off)
+- 6: Run the following commands
+- 7: Click on Preview at the top to show the web app
+- 8: Paste a review and enjoy!
+```bash
+# Clone the repository
+git clone https://github.com/maxpastor/Amazon-Review-Analysis.git
+pip install -r requirements.txt
+streamlit run app.py --server.port 8080
+```
+
+## How it works: 
+
+We get the review string, pass it to Amazon Bedrock and parse the XML output with a regex in order to extract the elements.
+
+The prompt is specifically asking for Claude to output the response in XML (which it is very good at doing), so that we can parse it easily.
+
+### And voila!
+
+Tweak things, play with the prompt and the temperature, and see how it goes 🤗 ! 
+
+
+
+
---
+++ app.py
@@ -0,0 +1,71 @@
+import streamlit as st
+import boto3
+import json 
+import re
+
+
+def parse_xml_reviews(xml_data):
+    positives = re.findall(r'<Positives>(.*?)</Positives>', xml_data, re.DOTALL)
+    neutrals = re.findall(r'<Neutrals>(.*?)</Neutrals>', xml_data, re.DOTALL)
+    negatives = re.findall(r'<Negatives>(.*?)</Negatives>', xml_data, re.DOTALL)
+
+    # Extract individual elements from the captured groups
+    positive_elements = re.findall(r'<Element>(.*?)</Element>', positives[0]) if positives else []
+    neutral_elements = re.findall(r'<Element>(.*?)</Element>', neutrals[0]) if neutrals else []
+    negative_elements = re.findall(r'<Element>(.*?)</Element>', negatives[0]) if negatives else []
+
+    return positive_elements, neutral_elements, negative_elements
+
+def parse_review(review):
+    bedrock = boto3.client(service_name='bedrock-runtime', region_name="us-west-2")
+    
+    modelId = 'anthropic.claude-v2'
+    accept = 'application/json'
+    contentType = 'application/json'
+    prompt = "\n\nHuman: You are a shopping assistant designed by Amazon. You are reading reviews and provide a list of the positive, neutral and negative aspects of the product. The goal is to help customers decide if they want to buy it or not. Each element of the list should be maximum 3 words. You must answer using this XML format. <Positives><Element>XXX</Element><Neutrals>... <Negatives>...  Always write  the elements in English\n\nAssistant: Understood"
+    prompt += "\n\nHuman: Amazon Review: "+review+"\n\nAssistant:"
+    body = json.dumps({
+            "prompt": prompt,
+            "max_tokens_to_sample": 8000,
+            "temperature": 0.1,
+            "top_p": 1,
+        })
+    response = bedrock.invoke_model(body=body, modelId=modelId, accept=accept, contentType=contentType)
+    response_body = json.loads(response.get('body').read())
+    return parse_xml_reviews(response_body.get("completion"))
+
+def main():
+    st.title("Amazon Review Analyzer")
+
+    # Text area for input
+    review_text = st.text_area("Paste an Amazon Review:", height=150)
+    
+    # Submit button
+    if st.button("Analyze Review"):
+        if review_text:
+            # Call your parse_review function
+            positive,neutral,negative = parse_review(review_text)
+            col1, col2, col3 = st.columns(3)
+
+            # Displaying the positives in the first column
+            with col1:
+                st.subheader("Positives:")
+                for item in positive:
+                    st.success(item)
+
+            # Displaying the neutrals in the second column
+            with col2:
+                st.subheader("Neutrals:")
+                for item in neutral:
+                    st.warning(item)
+
+            # Displaying the negatives in the third column
+            with col3:
+                st.subheader("Negatives:")
+                for item in negative:
+                    st.error(item)
+        else:
+            st.error("Please paste a review to analyze.")
+
+if __name__ == "__main__":
+    main()
